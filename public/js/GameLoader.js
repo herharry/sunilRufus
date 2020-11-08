f7929cb564f0596d2315ad77331a52554c73889d
Sunil-Rufus <Sunil-Rufus@>
2020-11-08 14:32:34 +0530

    starting

--- public/js/GameLoader.js
+++ public/js/GameLoader.js
@@ -1,23 +1,19 @@
-function fetchGame()
-{
-    fetch("/games")
-        .then(res => res.json())
-        .then(res => this.loadGameInCard(this.formatResponse(res)))
-        .catch(err => err);
-}
+var json = JSON.parse("[{\r\n  \"id\":1,\r\n  \"movie\" : \"Anniyan\",\r\n  \"url\" : \"https:\/\/in.bmscdn.com\/iedb\/movies\/images\/mobile\/thumbnail\/large\/anniyan-et00002333-24-03-2017-16-17-18.jpg\",\r\n  \"genre\" : [\"suspense\", \"thriller\"],\r\n  \"rating\" : 4,\r\n  \"date\" : \"2020-01-01\"\r\n},\r\n\r\n{\r\n  \"id\":2,\r\n  \"movie\" : \"Banyan\",\r\n  \"url\" : \"https:\/\/in.bmscdn.com\/iedb\/movies\/images\/mobile\/thumbnail\/large\/anniyan-et00002333-24-03-2017-16-17-18.jpg\",\r\n  \"genre\" : [\"comedy\"],\r\n  \"rating\" : 2,\r\n  \"date\" : \"2020-02-02\"\r\n},\r\n\r\n{\r\n  \"id\":3,\r\n  \"movie\" : \"Saniyan\",\r\n  \"url\" : \"https:\/\/in.bmscdn.com\/iedb\/movies\/images\/mobile\/thumbnail\/large\/anniyan-et00002333-24-03-2017-16-17-18.jpg\",\r\n  \"genre\" : [\"suspense\"],\r\n  \"rating\" : 1,\r\n  \"date\" : \"2020-03-03\"\r\n},\r\n\r\n{\r\n  \"id\":4,\r\n  \"movie\" : \"jsdbfhbsd\",\r\n  \"url\" : \"https:\/\/in.bmscdn.com\/iedb\/movies\/images\/mobile\/thumbnail\/large\/anniyan-et00002333-24-03-2017-16-17-18.jpg\",\r\n  \"genre\" : [\"horror\"],\r\n  \"rating\" : 4,\r\n  \"date\" : \"2020-04-04\"\r\n},\r\n\r\n{\r\n  \"id\":5,\r\n  \"movie\" : \"Suriya\",\r\n  \"url\" : \"https:\/\/in.bmscdn.com\/iedb\/movies\/images\/mobile\/thumbnail\/large\/anniyan-et00002333-24-03-2017-16-17-18.jpg\",\r\n  \"genre\" : [\"thriller\"],\r\n  \"rating\" : 4,\r\n  \"date\" : \"2020-05-05\"\r\n}\r\n]\r\n")
+// console.log(json);
+
 
 
 //todo action to create cards
-function loadGameInCard(games)
+function loadGameInCard()
 {
-    const parent = document.getElementById("cards");
-    for(let i=0;i<games.length;i++)
+    const parent = document.getElementById("movies");
+    for(let i=0;i<json.length;i++)
     {
-        console.log(games[i].gameImage);
+
         let card =document.createElement("div");
-        card.addEventListener("click", function() {fetchTournamentGames(games[i].gameID)},true);
+        card.addEventListener("click", function() {showMovieModal(json[i].id)},true);
         let img = document.createElement("img");
-        img.src = games[i].gameImage;
+        img.src = json[i].url;
         let card_im = document.createAttribute("class");
         card_im.value = "card-img-top card_img"
         img.setAttributeNode(card_im)
@@ -25,9 +21,18 @@ function loadGameInCard(games)
         att.value = "col-3 p-0 mr-4 bg-dark game-scroll-box  card";
         card.setAttributeNode(att);
         card.appendChild(img);
+        let p = document.createElement("p");
+        p.innerHTML = json[i].movie;
+        let al = document.createAttribute("class");
+        al.value = "align-middle"
+        p.setAttributeNode(al);
+        card.appendChild(p);
         parent.appendChild(card);
+
+
     }
 }
+
 
 
 function formatResponse(res)
@@ -39,12 +44,27 @@ function formatResponse(res)
     return ta;
 }
 
-function fetchTournamentGames(gameID)
+function showMovieModal(id)
 {
-    fetch("/tournament?gameID="+gameID)
-        .then(res => res.json())
-        .then(res => this.doGetGam(this.formatResponse(res)))
-        .catch(err => err);
+
+  console.log("ghdhg");
+
+  var modal = document.getElementById("myModal");
+
+  var span = document.getElementsByClassName("close")[0];
+
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
 
 function doGetGam(res)
